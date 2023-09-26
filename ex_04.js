f4580b069caa16bbb0a41971f26cb9066aebb7e9
nhhbut <146077193+nhhbut@users.noreply.github.com>
2023-09-26 17:01:20 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,4 +1,4 @@
-document.write ("Welcome to the javascript pool!")
+document.write ("Welcome to the javascript pool!")<br>
 document .write ("  Have some fun and don't forget to bring a towel.")
 
 console.log ("Website launched")
